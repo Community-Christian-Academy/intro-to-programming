aaafa65f4d551c10570c12ae8a4d684ad2087469
Braden Smith <DCSetup@CCA-HSLAB-11.CCA.Local.com>
2014-08-20 08:26:40 -0500

    Revert "Revert "Add The First Code Sample""
    This reverts commit 0683cecb7e16b23cdb50186621c3022a099f5d47.

---
+++ firstExample.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int half(int a) {
+    return a/2;
+}
+
+int timesTwo(int b) {
+    return b+b;
+}
+
+int main() {
+    printf("Hello, World!\n");
+    
+    
+    int x = 41;
+    printf("the number is:  %d\n", x);
+    
+    
+    int y = timesTwo( half(x) );
+    printf("my other number is %d\n", y);
+    
+    return 0;
+}
