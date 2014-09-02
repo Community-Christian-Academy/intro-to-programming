4d4f8b00cd904dbbb1cf4685c1660be1f3392783
Shannon Eckelkamp <shannoneckelkamp@Shannons-MacBook-Pro.local>
2014-09-02 17:56:46 -0500

    changed guessed to quashed

---
+++ ShannonsSourceFile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int MY_AWESOME_PIN = 6831;
+
+int testLogin(int pin) {
+    if (pin == MY_AWESOME_PIN) {
+        return 1; // this means 'yes'
+    }
+    return 0; // this means 'no'
+}
+
+int main(int argc, const char * argv[])
+{
+    int i;
+    for (i = 0; i <= 9999; i++) {
+        printf("let's try PIN:  %d\n", i);
+        if (testLogin(i)) {
+            printf("you quashed it!\n");
+        }
+    }
+    
+    return 0;
+}
