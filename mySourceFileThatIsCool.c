6ae438e4d94f375eba2780f32f65146ca9193391
Timothy T Hagood <Tanner@Timothys-MacBook-Pro.local>
2014-09-02 21:26:40 -0500

    successfully ran program

---
+++ mySourceFileThatIsCool.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int THE_PIN_OF_AWESOMENESS = 5643;
+
+int testLogin( int pin) {
+    if (pin == THE_PIN_OF_AWESOMENESS) {
+        return 1; // this means 'yes'
+        
+    }
+    return 0; // this means 'no'
+}
+
+int main( int argc, const char * argv [])
+{
+    int i;
+    for (i = 0; i <= 9999; i++) {
+        printf("Let's begin by quashing PIN:  %d\n", i);
+        if (testLogin(i)) {
+            printf("Quashing complete\n");
+        }
+    }
+    
+    return 0;
+}
