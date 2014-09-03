ead612fbe31ddb58089f5d3125f41ffafcb144c0
Braden Smith <lukkht@gmail.com>
2014-09-03 08:29:12 -0500

    added file
    diddy

---
+++ new file.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int MY_AWESOME_PIN = 6831
+
+int testlogin(int pin) {
+    if (pin == MY_AWESOME_PIN) {
+        return 1; // this means 'yes'
+    }
+    return 0; // this means 'no'
+}int main(int argc, const char * argv[])
+{
+     int i;
+     for (i=0; i<=9999; i++) {
+          printf("let's try PIN: %d\n", i);
+	  if (testlogin(i)) {
+	      printf("you guessed it!\n");
+          }
+     }
+
+     return 0
+}
