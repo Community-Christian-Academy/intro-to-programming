eba9c7a88defe65d338d3e3ff9cd95f24c8b5a24
Logan Browning <logan_80@comcast.net>
2014-09-03 08:16:33 -0500

    added MySourceFile

---
+++ MySourceFile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int MY_AWESOME_PIN = 2830;
+
+int testLogin(int pin) {
+    if (pin == MY_AWESOME_PIN) {
+        return 1; //this means 'yes'
+    }
+    return 0; //this means 'no'
+}
+
+int main(int argc, const char * argv[])
+{
+    int i;
+    for (i = 0; i <= 9999; i++) {
+        printf("let's try PIN: %d\n", i);
+        if (testLogin(i)) {
+            printf("You know it!\n");
+        }
+    }
+
+    return 0;
+}
