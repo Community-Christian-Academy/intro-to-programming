720ef22e212357ee8fa5c16e9fadb8ddcba17727
PrestonMuir <psmsoccer7@gmail.com>
2014-09-03 08:25:55 -0500

    un broken
    fixed

--- easy.c
+++ easy.c
@@ -4,8 +4,8 @@
 
 	int MY_AWESOME_PIN = 1506;
 
-	int testlogin(int pin) {
-	if (pin == MY_AWESOME_PIN){
+	int testLogin(int pin) {
+	if (pin == MY_AWESOME_PIN) {
 	return 1; // this means 'yes'
 }
 return 0; //this means 'no'
@@ -13,7 +13,7 @@ return 0; //this means 'no'
 int main(int argc, const char * argv[])
 {
 	int i;
-	for (i = 0; i <= 9999; i++);
+	for (i = 0; i <= 9999; i++) {
 	printf("Let's try this:   %d\n", i);
 	if (testLogin(i)) {
 	printf("quished \n");
